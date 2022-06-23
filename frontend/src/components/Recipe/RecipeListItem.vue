27fc196656ec511afd5bfdc67daa9aff54a99331
윤주혜 <yoonjuhye6@gmail.com>
2022-06-23 21:17:12 +0900

    [Front] : ADD_components | 컴포넌트 추가

---
+++ frontend/src/components/Recipe/RecipeListItem.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    
+  </div>
+</template>
+
+<script>
+import router from '@/router'
+
+export default {
+    name: 'MovieListItem',
+    props:{
+        movie:{
+            type:Object,
+        }
+    },
+    methods:{
+
+    }
+}
+</script>
+
+<style>
+
+</style>
