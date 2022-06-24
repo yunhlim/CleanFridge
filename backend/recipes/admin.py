e09a74430842e46099bc73ba176040ca0f2e48bb
yunh <yunh04131@gmail.com>
2022-06-24 21:36:44 +0900

    [BACK] Recipe process django DB 설정 완료

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Recipe, RecipeIngredient
+from .models import Recipe, RecipeIngredient, RecipeProcess
 
 
 admin.site.register(Recipe)
 admin.site.register(RecipeIngredient)
+admin.site.register(RecipeProcess)
--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -29,3 +29,16 @@ class RecipeIngredient(models.Model):
     class Meta:
         managed = False
         db_table = 'recipe_ingredient'
+
+
+class RecipeProcess(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    recipe = models.ForeignKey(Recipe, models.DO_NOTHING)
+    order = models.IntegerField()
+    content = models.CharField(max_length=256)
+    image_url = models.CharField(max_length=64, blank=True, null=True)
+    tip = models.CharField(max_length=256, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'recipe_process'
