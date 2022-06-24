88e97c521502abdd6cd975f2816a8b09a60c8be7
yunh <yunh04131@gmail.com>
2022-06-25 01:11:46 +0900

    [BACK] 사용자 재료 DB 완료

--- backend/accounts/admin.py
+++ backend/accounts/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User
+from .models import User, Ingredient
 
 admin.site.register(User, UserAdmin)
+admin.site.register(Ingredient)
 
 # Register your models here.
--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -4,3 +4,12 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     pass
+
+
+class Ingredient(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=30)
+
+    class Meta:
+        managed = False
+        db_table = 'ingredient'
