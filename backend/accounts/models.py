24cc853893f93f7dc2ca3d76a4842c2232edeea2
yunh <yunh04131@gmail.com>
2022-06-28 01:40:20 +0900

    [BACK] user - recipe like 다대다 관계 설정

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from recipes.models import Recipe
 
 
 class Ingredient(models.Model):
@@ -13,6 +14,7 @@ class Ingredient(models.Model):
 
 class User(AbstractUser):
     ingredients = models.ManyToManyField(Ingredient, through='Fridge')
+    like_recipes = models.ManyToManyField(Recipe, related_name='like_users')
 
 
 class Fridge(models.Model):
