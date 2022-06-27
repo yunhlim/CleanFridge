90980c44930f32e12e10e6e195b9f135d7f93c1b
yunh <yunh04131@gmail.com>
2022-06-27 23:50:51 +0900

    [BACK] 냉장고 DB 설정

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -2,10 +2,6 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
-class User(AbstractUser):
-    pass
-
-
 class Ingredient(models.Model):
     id = models.BigAutoField(primary_key=True)
     name = models.CharField(max_length=30)
@@ -13,3 +9,13 @@ class Ingredient(models.Model):
     class Meta:
         managed = False
         db_table = 'ingredient'
+
+
+class User(AbstractUser):
+    ingredients = models.ManyToManyField(Ingredient, through='Fridge')
+
+
+class Fridge(models.Model):
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    expiration_date = models.DateField()
