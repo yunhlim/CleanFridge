64d14f6a38b251b78bac2a462ed8eefba5f3efc0
yunh <yunh04131@gmail.com>
2022-07-06 22:34:43 +0900

    [BACK] profile 냉장고 재료 보여주는 기능 추가

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,14 +1,19 @@
 from django.shortcuts import get_list_or_404
+from django.contrib.auth import get_user_model
 
 from .models import Ingredient
-from .serializers import IngredientsSerializer
+from .serializers import IngredientsSerializer, ProfileSerializer
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
-# Create your views here.
+User = get_user_model()
 
+@api_view(['GET'])
 def profile(request):
-    pass
+    user = request.user
+    print(user)
+    serializer = ProfileSerializer(user)
+    return Response(serializer.data)
 
 
 @api_view(['GET'])
